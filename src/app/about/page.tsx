38ea55ba3d347aef55070a60c20142cf1e739f3d
Fredrik Johansen <fredrik1206@gmail.com>
2023-12-23 12:16:35 +0100

    fix(about): fixes profile picture on about me page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,7 +7,7 @@ export default function AboutPage() {
       <h1 className="text-light-header text-4xl dark:text-dark-header mb-2">
         Introduction👋
       </h1>
-      <section className="flex flex-row">
+      <section className="flex md:flex-row flex-col items-center">
         <p className="text-light-text text-base dark:text-dark-text">
           Hi I&apos;m Fredrik Johansen, a young, inspired self-taught software
           developer with 5 years of experience with building different types of
@@ -15,13 +15,18 @@ export default function AboutPage() {
           person, that&apos;s always ready for a new challenge/problem to solve
           to make an impact for other people.
         </p>
-        <Image
-          className="rounded-full ml-3"
-          src="https://avatars.githubusercontent.com/u/31306224?v=4"
-          alt="Image of me, Fredrik Johansen"
-          width={125}
-          height={125}
-        />
+        <div className="flex flex-col items-center my-4 md:my-0 md:ml-3 md:w-[70rem]">
+          <Image
+            className="rounded-full"
+            src="https://avatars.githubusercontent.com/u/31306224?v=4"
+            alt="Image of me, Fredrik Johansen"
+            width={125}
+            height={125}
+          />
+          <p className="italic dark:text-dark-text text-light-text mt-2">
+            - Picture of Fredrik Johansen
+          </p>
+        </div>
       </section>
       <h1 className="text-light-header text-4xl dark:text-dark-header mb-2">
         The Start🌱
