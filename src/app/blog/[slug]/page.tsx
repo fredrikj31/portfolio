f23acb3f1b7b810eafdd950bfcc99ee49779cb45
Fredrik Johansen <fredrik1206@gmail.com>
2023-12-23 12:03:16 +0100

    feat(seo): adds metadata tags on blog posts

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -3,18 +3,44 @@ import React from "react";
 import hljs from "highlight.js";
 import "highlight.js/styles/github-dark.css";
 import { parseHTML } from "@/src/utils/htmlParser";
-import { getBlogPost } from "@/src/services/hashnode";
+import { getBlogPost, getBlogPostSeo } from "@/src/services/hashnode";
+import { Metadata } from "next";
 
 // Highlight JS
 hljs.configure({
   classPrefix: "lang",
 });
 
-export default async function BlogPostPage({
-  params,
-}: {
+type Props = {
   params: { slug: string };
-}) {
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const blogPostSeo = await getBlogPostSeo({ slug: params.slug });
+
+  return {
+    title: blogPostSeo.seo.title || blogPostSeo.title,
+    category: "blog",
+    keywords: blogPostSeo.tags.map((tag) => tag.name),
+    metadataBase: new URL("https://fredrikjohansen.dev"),
+    publisher: "Fredrik Johansen",
+    authors: { name: "Fredrik Johansen", url: "/" },
+    description: blogPostSeo.seo.description || blogPostSeo.subtitle,
+    openGraph: {
+      type: "article",
+      authors: "Fredrik Johansen",
+      description: blogPostSeo.seo.description || blogPostSeo.subtitle,
+      title: blogPostSeo.seo.title || blogPostSeo.title,
+      images: [blogPostSeo.ogMetaData.image || blogPostSeo.coverImage],
+      publishedTime: blogPostSeo.publishedAt,
+      modifiedTime: blogPostSeo.updatedAt || undefined,
+      tags: blogPostSeo.tags.map((tag) => tag.name),
+      url: `/blog/${params.slug}`,
+    },
+  };
+}
+
+export default async function BlogPostPage({ params }: Props) {
   const blogPost = await getBlogPost({ slug: params.slug });
   const parsedHTML = parseHTML(blogPost.content.html);
 
