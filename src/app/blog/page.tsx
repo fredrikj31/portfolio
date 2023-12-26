7e0b7e371095f2fc628cf47816d5218317b7ee39
Fredrik Johansen <fredrik1206@gmail.com>
2023-12-26 23:40:00 +0100

    fix(blog-posts): replaces placeholder with actual blog posts

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,22 @@
-export default function BlogPage() {
+import { BlogPosts } from "@/src/components/home/BlogPosts";
+import { listBlogPosts } from "@/src/services/hashnode";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Blog - Fredrik Johansen",
+};
+
+export default async function BlogPage() {
+  const blogPosts = await listBlogPosts({ numberOfPosts: 10 });
+
   return (
     <>
-      <h1>Hi from blog page</h1>
+      <div className="mt-10">
+        <h1 className="text-4xl text-light-header dark:text-dark-header mb-3">
+          Blog Posts📝
+        </h1>
+        <BlogPosts blogPosts={blogPosts} />
+      </div>
     </>
   );
 }
