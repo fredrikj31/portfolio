34fab3e8956ca9ebbd8d53b82b2e669466734abb
Fredrik Johansen <fredrik1206@gmail.com>
2024-01-04 15:27:26 +0100

    fix(tailwind): adds utils folder and extends color theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,24 +5,26 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/utils/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   darkMode: "class",
   theme: {
-    colors: {
-      "light-background": "#FFFFFF",
-      "light-header": "#333333",
-      "light-text": "#555555",
-      "light-link": "#005A9E",
-      "dark-background": "#222222",
-      "dark-header": "#FFFFFF",
-      "dark-text": "#CCCCCC",
-      "dark-link": "#90B0D0",
+    extend: {
+      colors: {
+        "light-background": "#FFFFFF",
+        "light-header": "#333333",
+        "light-text": "#555555",
+        "light-link": "#005A9E",
+        "dark-background": "#222222",
+        "dark-header": "#FFFFFF",
+        "dark-text": "#CCCCCC",
+        "dark-link": "#90B0D0",
 
-      primary: "#3498db",
-      secondary: "#2ecc71",
-      highlight: "#f1c40f",
+        primary: "#3498db",
+        secondary: "#2ecc71",
+        highlight: "#f1c40f",
+      },
     },
-    extend: {},
     // Breakpoints
     screens: {
       sm: "376px",
