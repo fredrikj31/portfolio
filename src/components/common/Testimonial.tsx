966e4d71cd020eff3cf1f0c2d1f7ae59a448b3af
Fredrik Johansen <fredrik1206@gmail.com>
2024-01-05 11:06:46 +0100

    feat(testimonials): adds link to social

--- src/components/common/Testimonial.tsx
+++ src/components/common/Testimonial.tsx
@@ -4,13 +4,20 @@ interface TestimonialProps {
   testimonial: TestimonialOptions;
 }
 
-export const Testimonial = ({ testimonial: { author, company, position, testimonial } }: TestimonialProps) => {
+export const Testimonial = ({ testimonial: { author, company, position, social, testimonial } }: TestimonialProps) => {
   return (
     <div className="flex flex-col gap-4">
       <blockquote className="flex flex-col">
         <p className="text-light-text dark:text-dark-text italic mb-2">“{testimonial}”</p>
         <footer className="flex flex-col">
-          <cite className="text-light-text dark:text-dark-text not-italic">{author}</cite>
+          <div className="flex flex-row">
+            <cite className="text-light-text dark:text-dark-text not-italic">{author}</cite>
+            {social && (
+              <a href={social} target="_blank">
+                🔗
+              </a>
+            )}
+          </div>
           <span className="text-light-text dark:text-dark-text">
             {position} - {company}
           </span>
--- src/services/sanity/handlers/testimonial/schemas.ts
+++ src/services/sanity/handlers/testimonial/schemas.ts
@@ -4,6 +4,7 @@ export const TestimonialSchema = z.object({
   author: z.string(),
   position: z.string(),
   company: z.string(),
+  social: z.string().optional(),
   testimonial: z.string(),
 });
 export type Testimonial = z.infer<typeof TestimonialSchema>;
