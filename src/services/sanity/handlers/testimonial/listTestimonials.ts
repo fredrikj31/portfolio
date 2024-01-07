a88b74472006e05655f108f9322ff2d41e47e686
Fredrik Johansen <fredrik1206@gmail.com>
2024-01-07 12:31:46 +0100

    fix(testimonials): makes testimonials limit optional

--- src/services/sanity/handlers/testimonial/listTestimonials.ts
+++ src/services/sanity/handlers/testimonial/listTestimonials.ts
@@ -2,12 +2,12 @@ import { SanityClient } from "next-sanity";
 import { Testimonial, TestimonialSchema } from "./schemas";
 
 export interface ListTestimonialsOptions {
-  limit: number;
+  limit?: number;
 }
 
 export const listTestimonials = async (client: SanityClient, opts: ListTestimonialsOptions): Promise<Testimonial[]> => {
   const testimonials = await client.fetch(`
-    *[_type == 'testimonial'][0..${opts.limit - 1}]
+    *[_type == 'testimonial']${opts.limit ? `[0..${opts.limit - 1}]` : ""}
   `);
 
   const parsedTestimonials = TestimonialSchema.array().parse(testimonials);
