a53149841c8d63612b54c92d094fa2921a73e55d
Fredrik Johansen <fredrik1206@gmail.com>
2024-01-07 13:31:48 +0100

    fix(home): fixes grammar mistakes on home page

--- src/components/home/Introduction.tsx
+++ src/components/home/Introduction.tsx
@@ -6,15 +6,15 @@ export const Introduction = () => {
       <h1 className="text-4xl text-light-header dark:text-dark-header mb-3">Hello World👋</h1>
       <p className="text-light-text text-base dark:text-dark-text">
         I&apos;m a self-taught software developer who specializes in mostly in backend, but also a little less on
-        frontend development. I also write about different programming hrefpic over on my{" "}
+        frontend development. I also write about different programming topics over on my{" "}
         <Link href={"/blog"} className="text-light-link dark:text-dark-link underline">
           blog
         </Link>
-        . Hop over href my{" "}
+        . Hop over to my{" "}
         <Link href={"/portfolio"} className="text-light-link dark:text-dark-link underline">
           portfolio
         </Link>
-        , href see what I spend my off-hours working on. If you&apos;re more interested in my professional career, check
+        , to see what I spend my off-hours working on. If you&apos;re more interested in my professional career, check
         out my{" "}
         <Link href={"/resume"} className="text-light-link dark:text-dark-link underline">
           resume
