bc36a8e159d6666e02137266f410dccbfd45e83c
Fredrik Johansen <fredrik1206@gmail.com>
2024-01-16 19:57:33 +0100

    fix(robots.txt): removes blocked pages from robot.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,6 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: ["/favicon.ico", "/blog/sponsor"],
     },
     sitemap: "https://fredrikjohansen.dev/sitemap.xml",
   };
