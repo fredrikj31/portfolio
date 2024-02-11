c4660402e241a4297152901ccda1fedf7d756b0c
Fredrik Johansen <fredrik1206@gmail.com>
2024-02-11 16:23:30 +0100

    fix(page-container): adds full width on page container

--- src/components/PageContainer.tsx
+++ src/components/PageContainer.tsx
@@ -5,7 +5,7 @@ interface PageContainerProps {
 export default function PageContainer({ children }: PageContainerProps) {
   return (
     <div className="bg-white dark:bg-[#222222] min-h-screen transition-colors duration-500 px-4 py-4 md:py-10 flex flex-col">
-      <div className="mx-auto max-w-full md:max-w-2xl mg:max-w-3xl lg:max-w-4xl">{children}</div>
+      <div className="mx-auto w-full max-w-full md:max-w-2xl mg:max-w-3xl lg:max-w-4xl">{children}</div>
     </div>
   );
 }
