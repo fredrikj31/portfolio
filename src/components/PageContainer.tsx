adfc425ace840610c76aba0dc7b8a1ffb2a0253c
Fredrik Johansen <fredrik1206@gmail.com>
2024-02-11 16:00:15 +0100

    fix(page-container): fixes breakpoints for page container widths

--- src/components/PageContainer.tsx
+++ src/components/PageContainer.tsx
@@ -4,8 +4,8 @@ interface PageContainerProps {
 
 export default function PageContainer({ children }: PageContainerProps) {
   return (
-    <div className="bg-white dark:bg-[#222222] min-h-screen transition-colors duration-500 max-w-2xl mx-auto px-4 py-4 md:py-10 flex flex-col">
-      {children}
+    <div className="bg-white dark:bg-[#222222] min-h-screen transition-colors duration-500 px-4 py-4 md:py-10 flex flex-col">
+      <div className="mx-auto max-w-full md:max-w-2xl mg:max-w-3xl lg:max-w-4xl">{children}</div>
     </div>
   );
 }
