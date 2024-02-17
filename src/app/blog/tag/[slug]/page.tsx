8f9d49628fef19f7029e2590e6b56f523dca4802
Fredrik Johansen <fredrik1206@gmail.com>
2024-02-17 12:23:21 +0100

    fix(blog-tag): fixes headline for blog tag page

--- src/app/blog/tag/[slug]/page.tsx
+++ src/app/blog/tag/[slug]/page.tsx
@@ -18,9 +18,7 @@ export default async function BlogPostTagsPage({ params }: Props) {
   return (
     <>
       <div className="mt-10">
-        <h1 className="text-4xl text-light-header dark:text-dark-header mb-3">
-          Posts with tag &quot;{params.slug}&quot;📌
-        </h1>
+        <h1 className="text-4xl text-light-header dark:text-dark-header mb-3">Tagged: &quot;{params.slug}&quot;📌</h1>
         <div className="flex flex-col gap-5">
           {blogPosts.map((blogPost, index) => (
             <BlogPostPreview key={index} blogPostPreview={blogPost} />
