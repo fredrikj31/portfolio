dde60fe8b07bb71e579ba69b801ae2b932b1577d
Fredrik Johansen <fredrik1206@gmail.com>
2025-04-28 20:45:54 +0200

    feat(services): adds readTimeInMinutes to getProject Sanity query

--- src/services/sanity/handlers/project/schemas.ts
+++ src/services/sanity/handlers/project/schemas.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 export const ProjectSchema = z.object({
   title: z.string(),
   description: z.string(),
+  readTimeInMinutes: z.number().optional(),
   slug: z.string(),
   techstack: z.string().array(),
   content: z.any(),
