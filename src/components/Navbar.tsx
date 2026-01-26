15fe6769f8b315bc23327db6162ffd6797d341bb
Fredrik Johansen <fredrik1206@gmail.com>
2026-01-26 19:56:45 +0100

    fix(components): changes Navbar width to be 1280px

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -62,7 +62,7 @@ export const Navbar = () => {
 
   return (
     <header className="w-full border-b bg-background/95 print:hidden">
-      <div className="container flex h-14 items-center">
+      <div className="max-w-7xl mx-auto flex h-14 items-center">
         <div className="mr-4 hidden md:flex justify-between w-full">
           <Link href="/" className="mr-6 flex items-center space-x-2">
             <span className="hidden font-bold sm:inline-block">Fredrik Johansen</span>
