cfce5f6e4d5e20d04b58c3afba8636a76807fa07
Fredrik Johansen <fredrik1206@gmail.com>
2026-01-26 20:00:23 +0100

    fix(resume): removes margin top on resume print button

--- src/app/resume/components/PrintButton.tsx
+++ src/app/resume/components/PrintButton.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/shadcn/components/ui/button";
 export const PrintButton = () => {
   return (
     <div className="w-full print:hidden flex justify-end my-4">
-      <Button className=" w-fit mt-4" onClick={() => window.print()}>
+      <Button className="w-fit" onClick={() => window.print()}>
         Print Resume
       </Button>
     </div>
