1f1cd38af8e980aaf0b198c26a3486fa44a31f36
Fredrik Johansen <fredrik1206@gmail.com>
2026-01-30 21:21:17 +0100

    fix(components): adds padding on x-axis on mobile/tablet

--- src/components/PageContainer.tsx
+++ src/components/PageContainer.tsx
@@ -3,5 +3,5 @@ interface PageContainerProps {
 }
 
 export default function PageContainer({ children }: PageContainerProps) {
-  return <main className="max-w-5xl mx-auto h-full">{children}</main>;
+  return <main className="max-w-5xl mx-auto h-full px-4 lg:px-0">{children}</main>;
 }
