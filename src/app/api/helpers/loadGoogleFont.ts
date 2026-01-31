ff0df6379733b92c09b940cd84bffd60c0365f5b
Fredrik Johansen <fredrik1206@gmail.com>
2026-01-31 10:32:14 +0100

    feat(api): adds loadGoogleFont helper function to load font for OG image

---
+++ src/app/api/helpers/loadGoogleFont.ts
@@ -0,0 +1,17 @@
+export const loadGoogleFont = async (font: string): Promise<ArrayBuffer> => {
+  const url = `https://fonts.googleapis.com/css2?family=${font}`;
+  const css = await (await fetch(url)).text();
+  const resource = css.match(/src: url\((.+)\) format\('(opentype|truetype)'\)/);
+
+  console.log({ css });
+  console.log({ resource: resource?.[1] });
+
+  if (resource) {
+    const response = await fetch(resource[1]);
+    if (response.status == 200) {
+      return await response.arrayBuffer();
+    }
+  }
+
+  throw new Error("failed to load font data");
+};
